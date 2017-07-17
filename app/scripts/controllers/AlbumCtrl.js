9c25d5e91f57ec2d5cf9abb6f8fd8b37c582c67b
Your Name <you@example.com>
2017-07-17 11:40:14 -0700

    added Fixtures service checkpoint 6

--- app/scripts/controllers/AlbumCtrl.js
+++ app/scripts/controllers/AlbumCtrl.js
@@ -1,15 +1,14 @@
 //Create a controller for the Album view
 (function(){
-  function AlbumCtrl(){
-    this.albumData = angular.copy(albumPicasso);
+//Inject the custom Fixtures service into the controller
+  function AlbumCtrl(Fixtures){
+//Update AlbumCtrl to use the Fixtures service's getAlbum() method
+    this.albumData = Fixtures.getAlbum();
     this.songs = this.albumData.songs;
   }
 
   angular
     .module('blocJams')
-    .controller('AlbumCtrl', AlbumCtrl);
+//add Fixtures to AlbumCtrl's array of dependencies.
+    .controller('AlbumCtrl', ['Fixtures', AlbumCtrl]);
 })();
-
-
-
-<div class="collection-album-container column fourth" ng-repeat="album in collection.albums">
