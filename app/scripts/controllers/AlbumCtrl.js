cd4335eb27131754d5426692c1ae3449ee2995fc
Your Name <you@example.com>
2017-07-17 17:11:39 -0700

    added service to album checkpoint 7

--- app/scripts/controllers/AlbumCtrl.js
+++ app/scripts/controllers/AlbumCtrl.js
@@ -1,14 +1,15 @@
 //Create a controller for the Album view
 (function(){
 //Inject the custom Fixtures service into the controller
-  function AlbumCtrl(Fixtures){
+  function AlbumCtrl(Fixtures, SongPlayer){
 //Update AlbumCtrl to use the Fixtures service's getAlbum() method
     this.albumData = Fixtures.getAlbum();
-    this.songs = this.albumData.songs;
+//The songPlayer property holds the service and makes the service accessible within the Album view.
+    this.songPlayer = SongPlayer;
   }
 
   angular
     .module('blocJams')
 //add Fixtures to AlbumCtrl's array of dependencies.
-    .controller('AlbumCtrl', ['Fixtures', AlbumCtrl]);
+    .controller('AlbumCtrl', ['Fixtures', 'SongPlayer', AlbumCtrl]);
 })();
